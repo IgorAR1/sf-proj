0716902f1c3a6a6de9da155b895242bd63abbda9
Igoryan <abletonlive234@gmail.com>
2025-04-27 11:20:05 +0300

    init

--- src/Controller/ExampleController.php
+++ src/Controller/ExampleController.php
@@ -2,7 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ExampleController
 {
+    public function __construct(readonly EntityManagerInterface $em)
+    {
+    }
 
+    #[Route('example', name: 'example')]
+    public function index()
+    {
+        $post = new Post();
+        $post->setTitle('newTitle');
+        $post->setDescription('newDescription');
+
+        $this->em->persist($post);
+
+        $this->em->flush();
+
+        return new Response('done');
+    }
+
+    #[Route('show/{id}', name: 'show')]
+    public function show(Post $id)
+    {
+        $this->em->getRepository(Post::class)->findOneBySomeField($id)->setTitle('editTitle');
+    }
 }
---
+++ src/Repository/PostRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Post;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\VarExporter\LazyObjectInterface;
+
+/**
+ * @extends ServiceEntityRepository<Post>
+ */
+class PostRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Post::class);
+    }
+
+    /**
+     * @return Post[] Returns an array of Post objects
+     */
+    public function findById($value): array
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.id = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    public function findOneById($value): ?Post
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.id = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
